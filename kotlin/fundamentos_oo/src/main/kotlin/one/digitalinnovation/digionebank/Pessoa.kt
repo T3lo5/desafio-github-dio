be263267f9a4f5030ff9cd0bf9aa0f620ebea904
Anderson <andersonwsf@gmail.com>
2022-08-01 16:55:36 -0300

    novos repos

--- kotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ kotlin/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Potter"
-    var cpf: String = "123.456.789-00"
-    private set
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val potter = Pessoa()
-    println(potter.pessoaInfo())
-}
-
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
